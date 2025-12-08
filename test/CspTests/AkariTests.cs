0637018f9ac22c766a3c846c2d50ab706ab963d5
curtbrink <crtbrnk+github@gmail.com>
2025-12-07 21:01:26 -0500

    Add another akari test

--- test/CspTests/AkariTests.cs
+++ test/CspTests/AkariTests.cs
@@ -2,14 +2,16 @@ using Csp.Builders;
 using Csp.Gac;
 using Csp.Objects.Domain;
 using Csp.Objects.Variables.Interfaces;
+using Xunit.Abstractions;
 
 namespace CspTests;
 
-public class AkariTests
+public class AkariTests(ITestOutputHelper testOutputHelper)
 {
     [Fact]
     public void TestAkariGrid()
     {
+        // dailyakari.com/archive/334
         List<string> grid =
         [
             "www.ww.w.w",
@@ -38,6 +40,58 @@ public class AkariTests
         {
             var kDomain = solvedDomains[k].Values;
             Assert.Single(kDomain);
+        }
+
+        PrintGrid(grid, solvedDomains);
+    }
+    
+    [Fact]
+    public void TestAkariGridTwo()
+    {
+        // dailyakari.com/archive/333
+        List<string> grid =
+        [
+            "...............",
+            ".ww...1w...1w..",
+            "...0....3....2.",
+            ".0w...ww...12..",
+            "...w....3....w.",
+            ".ww...1w...1w..",
+            "...............",
+        ];
+
+        var akariCsp = AkariBuilder.Create().FromGrid(grid).Build();
+        
+        // working objects
+        var runConstraints = akariCsp.Constraints.ToList();
+        var runDomains = new Dictionary<IVariable, IDomain<int>>(akariCsp.Domains);
+        
+        var domainsToBacktrack = Gac.Run(runConstraints, runDomains);
+
+        var solvedDomains = Gac.RunWithBacktrackingSearch(runConstraints, domainsToBacktrack);
+
+        foreach (var k in solvedDomains.Keys)
+        {
+            var kDomain = solvedDomains[k].Values;
+            Assert.Single(kDomain);
+        }
+
+        PrintGrid(grid, solvedDomains);
+    }
+
+    private void PrintGrid(List<string> original, IDictionary<IVariable, IDomain<int>> domains)
+    {
+        var newGrid = original.Select(v => new string(v).ToCharArray().ToList()).ToList();
+
+        foreach (var (v, domain) in domains)
+        {
+            var gridCell = (IGridCellVariable)v;
+            newGrid[gridCell.Y][gridCell.X] = domain.Values.First() == 1 ? '@' : '.';
+        }
+
+        foreach (var row in newGrid)
+        {
+            testOutputHelper.WriteLine(new string(row.ToArray()));
         }
     }
 }
